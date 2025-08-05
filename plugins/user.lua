8138688f337d7c9ac26558ea44ce22ce8fb6a864
KaiyuWei <weikaiyu2020@outlook.com>
2025-08-05 13:41:33 +0200

    add config for status bar

--- plugins/user.lua
+++ plugins/user.lua
@@ -92,4 +92,25 @@ return {
     cmd = { "TodoQuickFix" }, -- commands that load this plugin
     keys = {}, -- key mappings that load this plugin
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup {
+        options = {
+          theme = "material", -- Use colorscheme theme
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { "filename" },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+      }
+    end,
+  },
 }
